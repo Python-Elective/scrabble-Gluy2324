a70c25b29be38638864eeaa8c8965448b8f9420d
Suppawat Rattanalor <suppawat.ra2025@kmids.ac.th>
2024-03-14 10:05:01 +0700

    done 3 work

--- Gluy_1103_Scrabble.py
+++ Gluy_1103_Scrabble.py
@@ -208,11 +208,22 @@ def is_valid_word(word: str, hand: dict , word_list: list) -> bool:
     word_list: list of lowercase strings
     """
     #checking pre condition
-    word : str
+    assert isinstance(word, str), "word must be a string"
     #    "hello".count('l')     ans = 2
 
-    if word not in word_list:
-        return False
+    """
+    psuedocode!
+
+    check if the word is in word_list or not
+    make a hand_copy
+    check for every letter in word
+        if those letter is in hand coppy and those letter is still usable
+            reduce those letter by 1
+        else
+            return false
+    if all passed return true
+    """
+
 
     hand_copy = hand.copy()
 
@@ -221,6 +232,72 @@ def is_valid_word(word: str, hand: dict , word_list: list) -> bool:
             hand_copy[letter] -= 1
         else:
             return False
-    
+    if word not in word_list:
+        return False
     return True
-    
+
+#testcase
+print(is_valid_word("hello", {'h':1, 'e':1, 'o':1, 'l':2}, ['hello'])) #ans = TRUE
+print(is_valid_word("hello", {'h':1, 'e':1, 'l':1, 'o':1}, ['hello'])) #ans = FALSE
+
+def calculate_hand_len(hand : dict):
+    """ 
+    Returns the length (number of letters) in the current hand.
+
+    hand: dictionary (string-> int)
+    returns: integer
+    """
+
+    len_hand = sum(hand.values())
+    return len_hand
+
+print(calculate_hand_len({'h':1, 'e':1, 'l':2, 'o':1})) #ans = 5
+
+def play_hand(hand, word_list, n):
+    """
+    Allows the user to play the given hand, as follows:
+
+    * The hand is displayed.
+    * The user may input a word or a single period (the string ".") 
+      to indicate they're done playing
+    * Invalid words are rejected, and a message is displayed asking
+      the user to choose another word until they enter a valid word or "."
+    * When a valid word is entered, it uses up letters from the hand.
+    * After every valid word: the score for that word is displayed,
+      the remaining letters in the hand are displayed, and the user
+      is asked to input another word.
+    * The sum of the word scores is displayed when the hand finishes.
+    * The hand finishes when there are no more unused letters or the user
+      inputs a "."
+
+      hand: dictionary (string -> int)
+      word_list: list of lowercase strings
+      n: integer (HAND_SIZE; i.e., hand size required for additional points)
+
+    """
+    # BEGIN PSEUDOCODE <-- Remove this comment when you code this function; do your coding within the pseudocode (leaving those comments in-place!)
+    # Keep track of the total score
+
+    # As long as there are still letters left in the hand:
+
+    # Display the hand
+
+    # Ask user for input
+
+    # If the input is a single period:
+
+    # End the game (break out of the loop)
+
+    # Otherwise (the input is not a single period):
+
+    # If the word is not valid:
+
+    # Reject invalid word (print a message followed by a blank line)
+
+    # Otherwise (the word is valid):
+
+    # Tell the user how many points the word earned, and the updated total score, in one line followed by a blank line
+
+    # Update the hand
+
+    # Game is over (user entered a '.' or ran out of letters), so tell user the total score
